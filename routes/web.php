1d23ba1d27db205b779bcc5d6a1ee675393ba651
Caroline Rosa <cpauladarosa@gmail.com>
2019-05-26 17:20:31 -0300

    Alterações até aula 15

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,13 @@ Route::get('/contato/{id?}', ['uses'=>'ContatoController@index']);
 Route::post('/contato', ['uses'=>'ContatoController@criar']);
 Route::put('/contato', ['uses'=>'ContatoController@editar']);
 
+Route::get('/admin/cursos',['as' => 'admin.cursos','uses' => 'Admin\CursoController@index']); //lista todos os cursos cadastrados, admin.cursos é apelido
+Route::get('/admin/adicionar',['as' => 'admin.cursos.adicionar','uses' => 'Admin\CursoController@adicionar']); 
+Route::post('/admin/salvar',['as' => 'admin.cursos.salvar','uses' => 'Admin\CursoController@salvar']); 
+Route::get('/admin/editar/{id}',['as' => 'admin.cursos.editar','uses' => 'Admin\CursoController@editar']); 
+Route::put('/admin/atualizar/{id}',['as' => 'admin.cursos.atualizar','uses' => 'Admin\CursoController@atualizar']); //atualizar o que for alterado no editar
+Route::get('/admin/deletar/{id}',['as' => 'admin.cursos.deletar','uses' => 'Admin\CursoController@deletar']);
+
 
 /*Route::post('/contato', function(){
     //var_dump($_POST);
